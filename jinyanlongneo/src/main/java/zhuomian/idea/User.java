b4835f3eb661b89e9eb645eb87c5b075fca25c9b
yangzai <1083291495@qq.com>
2018-12-27 23:47:54 +0800

    第二

--- jinyanlongneo/src/main/java/zhuomian/idea/User.java
+++ jinyanlongneo/src/main/java/zhuomian/idea/User.java
@@ -4,6 +4,7 @@ public class User {
     public static void main(String[] args) {
         System.out.println("i am really a kids,she were right!");
         System.out.println("i am truely a kids");
+        System.out.println("new ");
 
     }
 
